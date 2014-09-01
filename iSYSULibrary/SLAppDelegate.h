b721088cdd5b074da6fc9b9a6c61933729258e45
AlayshChen <alayshchen@gmail.com>
2014-09-01 14:00:20 +0800

    First commit

---
+++ iSYSULibrary/SLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  iSYSULibrary
+//
+//  Created by Alaysh on 9/1/14.
+//  Copyright (c) 2014 Alaysh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iSYSULibrary/SLViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLViewController.h
+//  iSYSULibrary
+//
+//  Created by Alaysh on 9/1/14.
+//  Copyright (c) 2014 Alaysh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLViewController : UIViewController
+
+@end
