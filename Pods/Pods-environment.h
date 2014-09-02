49de136209b9ffabe7476a37dd6eb19bb7157077
AlayshChen <alayshchen@gmail.com>
2014-09-02 15:43:22 +0800

    Pod install 3rd-party libraries

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// LBBlurredImage
+#define COCOAPODS_POD_AVAILABLE_LBBlurredImage
+#define COCOAPODS_VERSION_MAJOR_LBBlurredImage 0
+#define COCOAPODS_VERSION_MINOR_LBBlurredImage 2
+#define COCOAPODS_VERSION_PATCH_LBBlurredImage 0
+
+// SVProgressHUD
+#define COCOAPODS_POD_AVAILABLE_SVProgressHUD
+#define COCOAPODS_VERSION_MAJOR_SVProgressHUD 1
+#define COCOAPODS_VERSION_MINOR_SVProgressHUD 0
+#define COCOAPODS_VERSION_PATCH_SVProgressHUD 0
+
+// XDKAirMenu
+#define COCOAPODS_POD_AVAILABLE_XDKAirMenu
+#define COCOAPODS_VERSION_MAJOR_XDKAirMenu 1
+#define COCOAPODS_VERSION_MINOR_XDKAirMenu 1
+#define COCOAPODS_VERSION_PATCH_XDKAirMenu 0
+
