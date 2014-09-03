a23d08c7b6a23a944c18c43fd8ec4f8780927ef9
AlayshChen <alayshchen@gmail.com>
2014-09-04 00:00:53 +0800

    Fixing bug

--- iSYSULibrary/SLMainViewController.m
+++ iSYSULibrary/SLMainViewController.m
@@ -15,6 +15,7 @@
 
 @interface SLMainViewController ()
 @property (nonatomic, strong) UIBarButtonItem *backButtonItem;
+@property (nonatomic, assign) BOOL isOpen;
 @end
 
 @implementation SLMainViewController
@@ -34,6 +35,7 @@
     [self configureNavigationItem];
     self.collectionView.backgroundColor = kApplicationGrayColor;
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(modifyNavBarAlpha:) name:kNotificationModifyNavBarAlpha object:nil];
+    self.isOpen = false;
 }
 
 - (void)didReceiveMemoryWarning
@@ -93,58 +95,66 @@
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
-    CGRect frame = cell.frame;
-    frame.origin.y -= collectionView.contentOffset.y;
-    UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"%d", indexPath.row+1]];
-     _bookView = [[SLBookView alloc] initWithFrame:frame];
-    [_bookView setupBookCoverImage:image];
-    [self.view addSubview:_bookView];
-    SLBookDetailViewController *detailVC = [[SLBookDetailViewController alloc] init];
-    detailVC.modalTransitionStyle = UIModalTransitionStyleOpenBooks;
-    detailVC.bookCoverImage = image;
-    detailVC.bookInfo = @[
-                          @[@{
-                              @"bookCoverImageUrl": [NSString stringWithFormat:@"%d", indexPath.row + 1],
-                              @"bookName":  @"Objective-C高级编程 : iOS与OS X多线程和内存管理",
-                              @"bookId":    @"978-7-115-31809-1",
-                              @"bookAuthor":  @"(日) Kazuki Sakamoto, Tomohiko Furumoto著 ; 黎华译",
-                              @"bookPress":  @"人民邮电出版社",
-                              }],
-                          @[@{
-                              @"brief":  @"本书在苹果公司公开的源代码基础上，深入剖析了对应用于内存管理的ARC以及应用于多线程开发的Blocks和GCD。内容包括：自动引用计数、Blocks、Grand Central Dispatch等。"
-                              }],
-                          @[@{
-                                          @"bookState":    @"外借本",
-                                          @"dueDate":    @"在架上",
-                                          @"branch":     @"南校区中文新书库(1楼)",
-                                          @"rackPosition":       @"TP312C/1987",
-                                          @"requests":     @"0",
-                                          },
-                                      @{
-                                          @"bookState":    @"外借本",
-                                          @"dueDate":    @"20141008",
-                                          @"branch":     @"东校区普通图书(3楼)",
-                                          @"rackPosition":       @"TP312C/1987",
-                                          @"requests":     @"1"
-                                          }
-                                      ]
-                          ];
-    [self presentViewController:detailVC animated:YES completion:^{
-        self.title = @"图书详情";
-        self.navigationItem.leftBarButtonItem = self.backButtonItem;;
-        self.navigationItem.rightBarButtonItem = nil;
-    }];
+    if (!self.isOpen) {
+        UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
+        CGRect frame = cell.frame;
+        frame.origin.y -= collectionView.contentOffset.y;
+        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"%d", indexPath.row+1]];
+        _bookView = [[SLBookView alloc] initWithFrame:frame];
+        [_bookView setupBookCoverImage:image];
+        [self.view addSubview:_bookView];
+        SLBookDetailViewController *detailVC = [[SLBookDetailViewController alloc] init];
+        detailVC.modalTransitionStyle = UIModalTransitionStyleOpenBooks;
+        detailVC.bookCoverImage = image;
+        detailVC.bookInfo = @[
+                              @[@{
+                                    @"bookCoverImageUrl": [NSString stringWithFormat:@"%d", indexPath.row + 1],
+                                    @"bookName":  @"Objective-C高级编程 : iOS与OS X多线程和内存管理",
+                                    @"bookId":    @"978-7-115-31809-1",
+                                    @"bookAuthor":  @"(日) Kazuki Sakamoto, Tomohiko Furumoto著 ; 黎华译",
+                                    @"bookPress":  @"人民邮电出版社",
+                                    }],
+                              @[@{
+                                    @"brief":  @"本书在苹果公司公开的源代码基础上，深入剖析了对应用于内存管理的ARC以及应用于多线程开发的Blocks和GCD。内容包括：自动引用计数、Blocks、Grand Central Dispatch等。"
+                                    }],
+                              @[@{
+                                    @"bookState":    @"外借本",
+                                    @"dueDate":    @"在架上",
+                                    @"branch":     @"南校区中文新书库(1楼)",
+                                    @"rackPosition":       @"TP312C/1987",
+                                    @"requests":     @"0",
+                                    },
+                                @{
+                                    @"bookState":    @"外借本",
+                                    @"dueDate":    @"20141008",
+                                    @"branch":     @"东校区普通图书(3楼)",
+                                    @"rackPosition":       @"TP312C/1987",
+                                    @"requests":     @"1"
+                                    }
+                                ]
+                              ];
+        self.isOpen = true;
+        [self presentViewController:detailVC animated:YES completion:^{
+            self.title = @"图书详情";
+            self.navigationItem.leftBarButtonItem = self.backButtonItem;
+            self.navigationItem.rightBarButtonItem = nil;
+        }];
+    }
+    
 }
 
 - (void)closeBookDetailVC
 {
-    [self dismissViewControllerAnimated:true completion:^{
-        self.title = @"首页";
-        [_bookView removeFromSuperview];
-        _bookView = nil;
-        [self configureNavigationItem];
-    }];
+    if (self.isOpen) {
+        [self dismissViewControllerAnimated:true completion:^{
+            self.title = @"首页";
+            [_bookView removeFromSuperview];
+            _bookView = nil;
+            [self configureNavigationItem];
+            self.isOpen = false;
+        }];
+    }
+    
 }
 
 - (void)modifyNavBarAlpha:(NSNotification *)aNotification
