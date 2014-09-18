07d281524f6ef65661387e037dd3097a9b731337
AlayshChen <chenhb36@mail2.sysu.edu.cn>
2014-09-18 17:11:58 +0800

    Revert SLBorrowCell

--- iSYSULibrary/SLBorrowCell.h
+++ iSYSULibrary/SLBorrowCell.h
@@ -10,8 +10,6 @@
 
 @interface SLBorrowCell : UITableViewCell
 
-@property (weak, nonatomic) IBOutlet UIButton *circle;
-
 @property (strong, nonatomic) UIImageView *cover;      /* 书的封面 */
 @property (strong, nonatomic) UILabel *name;           /* 书名 */
 @property (strong, nonatomic) UILabel *borrowDate;     /* 借书日期 */
